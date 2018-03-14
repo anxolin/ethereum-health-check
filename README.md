8803de28f6780115249b1e1799aaf2b75b6b7d20
Angel Rodriguez <anxolin@gmail.com>
2018-03-14 15:50:06 +0100

    Check peer count > 0

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # Ethereum Health Check
 Checks the state of a RPC node in ethereum
 
+Install dependencies:
 ```bash
 python3 -m venv ENV
 source ENV/bin/activate
 pip install -r requirements.txt
+```
+
+Run check:
+```bash
+URL=https://kovan.infura.io/DyTLJKrhta2BDMSZ9MaV python check-health.py
 ```
--- check-health.py
+++ check-health.py
@@ -1,1 +1,32 @@
-print('Check Health')
+import json
+import web3
+import os
+import logging
+import sys
+
+from web3 import Web3, HTTPProvider
+
+SUCCESS_CODE = 0
+ERROR_CODE_NOT_ENOUGH_PEERS = 1
+
+# Logger
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
+url = os.environ['URL']
+if not url:
+  raise ValueError("The environment varianle 'URL' is required")
+
+
+logger.debug('Checking Health for %s', url)
+w3 = Web3(HTTPProvider(url))
+
+peer_count = w3.net.peerCount
+if peer_count == 0:
+  logger.error("There are any peers")
+  sys.exit(ERROR_CODE_NOT_ENOUGH_PEERS)
+else:
+  logger.debug("There are enough peers: %d", peer_count)
+  exit(SUCCESS_CODE)
